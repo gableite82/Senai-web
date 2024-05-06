87bdfb69f745d488f1360638ae0941757e5852fe
Gabriel Leite <gabrielleite82@hotmail.com>
2024-05-06 19:14:25 -0300

    new tesc

--- js/javascript.js
+++ js/javascript.js
@@ -1,7 +1,18 @@
-function entrar(){
-    let username = document.getElementById('username').value
-    let password = document.getElementById('password').value
 
-    console.log("Nome do usuário: " + username)
-    console.log("Senha do usuário: " + password)
+function salvarCadastro() {
+    var nomeProprietario = document.getElementById('nomeProprietario').value;
+    var numeroApartamento = document.getElementById('numeroApartamento').value;
+    var blocoApartamento = document.getElementById('blocoApartamento').value;
+    var placaVeiculo = document.getElementById('placaVeiculo').value;
+    var modeloVeiculo = document.getElementById('modeloVeiculo').value;
+    var vagaVeiculo = document.getElementById('vagaVeiculo').value;
+
+    console.log("Nome do Proprietário:", nomeProprietario);
+    console.log("Número do Apartamento:", numeroApartamento);
+    console.log("Bloco do Apartamento:", blocoApartamento);
+    console.log("Placa do Veículo:", placaVeiculo);
+    console.log("Modelo do Veículo:", modeloVeiculo);
+    console.log("Número da Vaga de Estacionamento:", vagaVeiculo);
+
+    alert("Cadastro realizado com sucesso!");
 }
